ea528627b7f3fb724a9f9a569aa573e6fafd4bc0
Anton-Euro <silikonov15@mail.ru>
2021-04-13 21:30:42 +0300

    new

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,74 @@
-jQuery('document').ready(function() {
-	jQuery(window).resize(function(e) {
-		let width = e.target.innerWidth
-		if(width < 1200) {
-
-		}
-	});
-});
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [53.26208809, 28.64121338],
+        zoom: 13,
+        controls: ['zoomControl']
+    });
+    var placemark_1 = new ymaps.Placemark([53.31500795, 28.64318814], {
+        balloonContentHeader: 'Сумченко Степан Сергеевич',
+        balloonContent: "<div id='text_br'>Сумченко Степан Сергеевич - В 1928 году Степан Сумченко был призван в Красную Армию. <br><a href='1.html' id='next'>Подробнее...</a></div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_1);
+    var placemark_2 = new ymaps.Placemark([53.30841212, 28.63420868], {
+        balloonContentHeader: 'Борис Михайлович Дмитриев',
+        balloonContent: "<div id='text_br'>Борис Михайлович Дмитриев (11 июня 1924 - 23 февраля 1944) — партизан Великой Отечественной войны, Герой Советского Союза (1944). <br><a href='2.html' id='next'>Подробнее...</a></div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_2);
+    var placemark_3 = new ymaps.Placemark([53.30871697, 28.64157221], {
+        balloonContentHeader: 'Римма Владимировна Кунько',
+        balloonContent: "<div id='text_br'>Римма Владимировна Кунько (14 декабря 1926 года — 1 июля 1944 года) — партизанка, участница партизанского движения на территории Могилёвской области Белорусской ССР в годы Великой Отечественной войны. <br><a href='3.html' id='next'>Подробнее...</a></div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_3);
+    var placemark_4 = new ymaps.Placemark([53.30635665, 28.64452638], {
+        balloonContentHeader: 'Николай Филиппович Королёв',
+        balloonContent: "<div id='text_br'>Николай Филиппович Королёв (01 июля 1906, д. Аминовичи, Могилёвская губерния, Российская империя — 07 апреля 1972, Могилёв, СССР) — советский военный и партийный деятель, генерал-майор (1943), Герой Советского Союза (1970).</div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_4);
+    var placemark_5 = new ymaps.Placemark([53.30626823, 28.64116264], {
+        balloonContentHeader: 'Каданчик Сергей Николаевич',
+        balloonContent: "<div id='text_br'>Каданчик Сергей Николаевич (12 сентября 1906 — 15 сентября 1943) — советский офицер, командир 1339-го стрелкового полка 318-й дивизии 18-й армии, подполковник. <br><a href='5.html' id='next'>Подробнее...</a></div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_5);
+    var placemark_6 = new ymaps.Placemark([53.30159582, 28.66239669], {
+        balloonContentHeader: 'Фёдор Андреевич Крылович',
+        balloonContent: "<div id='text_br'>Фёдор Андреевич Крыло́вич (23.03.1916, Минск, Российская империя — 07.11.1959, Осиповичи, Могилёвская область, БССР, СССР) — советский партизан, один из организаторов Осиповичского коммунистического подполья, руководитель партизанской диверсионной группы в годы Великой Отечественной в Белоруссии. <br><a href='6.html' id='next'>Подробнее...</a></div>",
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/pm.png',
+        iconImageSize: [47, 67],
+        iconImageOffset: [-19, -60],
+        balloonMaxWidth: 500
+    });
+    myMap.geoObjects.add(placemark_6);
+}
